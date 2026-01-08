962981d6a2d077ab2702c41b67730f84a09d2c3e
Zobeewan <revoire.christian@gmail.com>
2026-01-08 14:41:30 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,30 +1,35 @@
 class SimConfig:
     # --- Space & Time ---
-    Nx: int = 300                 # Grid size
-    Ny: int = 300                 # Only use on 2D simulation
+    Nx: int = 300                 # Grid size (x)
+    Ny: int = 300                 # Grid size (y), Only use on 2D simulation
+    
     x_min: float = -50.0          # Spatial extent (left)
     x_max: float = 50.0           # Spatial extent (right)
+    
+    y_min: float = -50.0          # Spatial extent (left)
+    y_max: float = 50.0           # Spatial extent (right)
+    
     dt: float = 0.01              # Time step
     
     # --- Simulation Control ---
     N_steps: int = 40000          # Steps per particle
     thermalization: int = 10000   # Steps to ignore (warmup)
-    N_runs: int = 1200            # Number of independent particles
+    N_runs: int = 1200            # Number of independent simulated particles
     N_CORES: int = -1             # 0 = use all available cores, -1 = keep 1 cores free (or more)
-    SUBSAMPLE: int = 1            # 1 = keep all points, 10 = keep 1/10 (avoid use if possible)
+    SUBSAMPLE: int = 1            # 1 = keep all points, 10 = keep 1 point out of 10 (avoid use if possible)
     
     # --- Wave Physics (Pilot Field) ---
     # Equation: ∂t ψ = (Dψ + iω)∇²ψ - γψ + Source
-    c: float = 1.0                # Propagation speed (normalized)
-    gamma: float = 0.02           # Dissipation (memory limit)
+    c: float = 1.0                # Propagation speed (c = 1 by choice of units, ideally should match the discretization dx/dt)    
+    gamma: float = 0.02           # Dissipation (system memory)
     D_psi: float = 0.9            # Spatial diffusion
-    emit_amp: float = 0.57        # Source amplitude
-    sigma_emit: float = 1.0       # Source width (will be scaled by dx)
-    omega: float = 2.0            # Dispersion frequency (analog to ℏ/2m)
+    emit_amp: float = 0.57        # Source emission amplitude
+    sigma_emit: float = 1.0       # Spatial width of the source (will be scaled by dx)
+    omega: float = 2.0            # Dispersive frequency (analog to ℏ/2m)
     
     # --- Particle Physics ---
     # Equation: dx = (α ⋅ ∇φ) dt + noise
-    alpha: float = 4.0            # Coupling strength (Inertial factor, analog k/m, equal to 2ω mean k=1)
+    alpha: float = 4.0            # Coupling strength (Inertial factor, analog k/m, equal to k*2ω)
     D_x: float = 0.28             # Stochastic diffusion (Brownian noise)
     epsilon: float = 1e-3         # Regularization factor for guidance
     
